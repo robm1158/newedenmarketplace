dceee1f86af3e31b6131213b01be6fd0c27d4bc4
robm1158 <mullins097956@gmail.com>
2023-08-25 01:02:25 -0600

    adding more functions

--- itemPrices.py
+++ itemPrices.py
@@ -12,5 +12,16 @@ def getAllItemOrderHistory(type_id: int,region_id=10000002) -> Dict:
     response = requests.get(f"https://esi.evetech.net/latest/markets/{region_id}/orders/?datasource=tranquility&order_type=all&page=1&type_id={type_id}")
     return json.dumps(response.json())
 
-
-print(getAllItemOrderHistory(34))
+# This will need to be turned into an async function and even recursive async
+def getRegionOrderIds(region_id=10000002) -> list:
+    pageNum = 1
+    idList = []
+    response = requests.get(f"https://esi.evetech.net/latest/markets/{region_id}/types/?datasource=tranquility&page={pageNum}")
+    while response != 404:
+        response = requests.get(f"https://esi.evetech.net/latest/markets/{region_id}/types/?datasource=tranquility&page={pageNum}")
+        idList.append(response.json())
+        pageNum+=1
+    print(pageNum)
+    print(len(pageNum))
+    return idList
+print(getRegionOrderIds())
