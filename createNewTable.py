f52c5dd5e3f0564738eb58466b53beaed2a5ffd0
robm1158 <mullins097956@gmail.com>
2023-08-25 13:39:43 -0600

    Update createNewTable.py
    adding in table creation for item orders

--- createNewTable.py
+++ createNewTable.py
@@ -16,18 +16,18 @@ class CreateNewTable:
                 KeySchema=[
                     {
                         'AttributeName': 'date',
-                        'KeyType': 'HASH'  # Sort Key
+                        'KeyType': 'HASH'  # Hash Key
                     }
                 ],
                 AttributeDefinitions=[
                     {
                         'AttributeName': 'date',
-                        'AttributeType': 'S'  # number data type
+                        'AttributeType': 'S'  # string data type
                     }
                 ],
                 ProvisionedThroughput={
-                    'ReadCapacityUnits': 10,
-                    'WriteCapacityUnits': 10
+                    'ReadCapacityUnits': 1,
+                    'WriteCapacityUnits': 50
                 }
             )
             print("[createNewTable]: New table created")
@@ -37,4 +37,42 @@ class CreateNewTable:
                 print(f"[createNewTable]: {err.response['message']} Table already created")
             else:
                 print(f"[createNewTable]: {err.response['message']}")
+            return err.response['ResponseMetadata']['HTTPStatusCode']
+            
+    def createItemOrderTable(self, tableName: str) -> int:
+        try:
+            response = self.dynamodb.create_table(
+                TableName=tableName,
+                KeySchema=[
+                    {
+                        'AttributeName': 'date', # It is called issued in the esi pull
+                        'KeyType': 'HASH'  # Hash Key
+                    },
+                    {
+                        'AttributeName': 'order_id', 
+                        'KeyType': 'RANGE'  # Sort Key
+                    }
+                ],
+                AttributeDefinitions=[
+                    {
+                        'AttributeName': 'date',
+                        'AttributeType': 'S'  # string data type
+                    },
+                    {
+                        'AttributeName': 'order_id',
+                        'AttributeType': 'N'  # string data type
+                    }
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 1,
+                    'WriteCapacityUnits': 50
+                }
+            )
+            print(f'[createNewTable]: New table {tableName} created')
+            return 200
+        except ClientError  as err:
+            if err.response['ResponseMetadata']['HTTPStatusCode'] == 400:
+                print(f"[createNewTable]: {err.response['message']} Table already created")
+            else:
+                print(f"[createNewTable]: Error msg {err.response['message']}")
             return err.response['ResponseMetadata']['HTTPStatusCode']
