9a05922e670b6af91509440f51266f8320b8caba
robm1158 <mullins097956@gmail.com>
2023-08-25 00:53:00 -0600

    creating basis for fetching item prices

--- itemPrices.py
+++ itemPrices.py
@@ -1,14 +1,16 @@
 from mimetypes import init
 from typing import Dict
 import requests
+import json
 
 
 def getItemsPriceHistory(type_id: int,region_id=10000002) -> Dict:
     response = requests.get(f"https://esi.evetech.net/latest/markets/{region_id}/history/?datasource=tranquility&type_id={type_id}")
-    return response.json()
+    return json.dumps(response.json())
 
-def sendItemPriceHistory(self):
-    #ToDo
-    return 0
+def getAllItemOrderHistory(type_id: int,region_id=10000002) -> Dict:
+    response = requests.get(f"https://esi.evetech.net/latest/markets/{region_id}/orders/?datasource=tranquility&order_type=all&page=1&type_id={type_id}")
+    return json.dumps(response.json())
 
-print(getItemsPriceHistory(34))
+
+print(getAllItemOrderHistory(34))
