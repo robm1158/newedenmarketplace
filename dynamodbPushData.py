4129a34de5b7e6fd654041b2cd7a7c8db0867de7
robm1158 <mullins097956@gmail.com>
2023-08-25 20:32:43 -0600

    added in second table for orders

--- dynamodbPushData.py
+++ dynamodbPushData.py
@@ -39,26 +39,33 @@ class pushData:
         print(f'Finished pushing the {tableName} table')
 
     def pushItemOrdersToDynamo(self,tableName: str)->None:
-        response = self.newTable.createItemOrderTable(tableName)
+        stringName = tableName+"_ORDERS"
+        response = self.newTable.createItemOrderTable(stringName)
         print(response)
         if response == 200:
-            print(f'Create new table {tableName}')
-            table = self.dynamodb.Table(tableName)
+            print(f'Create new table {stringName}')
+            table = self.dynamodb.Table(stringName)
             table.wait_until_exists()
             print("Done waiting")
             itemjson = itemPrices.getAllItemOrderHistory(item[tableName].value,region.THE_FORGE.value)
             jsondata = json.loads(itemjson, parse_float=Decimal)
-            with table.batch_writer() as batch:
-                for myDict in jsondata:
-                    print(myDict)
-                    response = batch.put_item(Item = myDict)
+            try:
+                with table.batch_writer() as batch:
+                    for myDict in jsondata:
+                        response = batch.put_item(Item = myDict)
+   
+            except Exception as e:
+                print(f"An exception occurred: {e}, {e.args}")
         else:
             print("Just update the table")
             table = self.dynamodb.Table(tableName)
             itemjson = itemPrices.getAllItemOrderHistory(item[tableName].value,region.THE_FORGE.value)
             jsondata = json.loads(itemjson, parse_float=Decimal)
-            with table.batch_writer() as batch:
-                for myDict in jsondata:
-                    print(myDict)
-                    response = batch.put_item(Item = myDict)
-        print(f'Finished pushing the {tableName} table')
+            try:
+                with table.batch_writer() as batch:
+                    for myDict in jsondata:
+                        response = batch.put_item(Item = myDict)
+
+            except Exception as e:
+                print(f"An exception occurred: {e}, {e.args}")
+        print(f'Finished pushing the {tableName}_ORDERS table')
