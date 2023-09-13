737946419fc5362e340e6e5ef3839c2bab753dcd
robm1158 <mullins097956@gmail.com>
2023-09-12 23:37:27 -0600

    trying to turn into async functions

--- dynamodbPushData.py
+++ dynamodbPushData.py
@@ -1,8 +1,7 @@
-import boto3
 import json
-import requests
-from typing import Dict
-from decimal import Decimal
+import asyncio
+import aioboto3
+from concurrent.futures import ThreadPoolExecutor
 from createNewTable import CreateNewTable
 import itemPrices
 from ItemIdEnum import item
@@ -11,58 +10,56 @@ import logging
 
 class pushData:
     def __init__(self) -> None:
-        self.dynamodb = boto3.resource('dynamodb')
+        self.dynamodb = aioboto3.resource('dynamodb')
         self.newTable = CreateNewTable()
 
-    def pushPriceHistoryToDynamo(self,tableName: str)->None:
-        response = self.newTable.createPriceHistoryTable(tableName)
+    async def pushPriceHistoryToDynamo(self, tableName: str) -> None:
+        loop = asyncio.get_event_loop()
+        response = await loop.run_in_executor(None, self.newTable.createPriceHistoryTable, tableName)
+
         if response == 200:
             logging.info(f'Create new table {tableName}')
-            table = self.dynamodb.Table(tableName)
-            table.wait_until_exists()
+            table = await self.dynamodb.Table(tableName)
+            await table.wait_until_exists()
             logging.info("Done waiting")
-            itemjson = itemPrices.getItemsPriceHistory(item[tableName].value,region.THE_FORGE.value)
-            jsondata = json.loads(itemjson, parse_float=Decimal)
-            with table.batch_writer() as batch:
+            itemjson = await itemPrices.getItemsPriceHistory(item[tableName].value, region.THE_FORGE.value)
+            jsondata = json.loads(itemjson)
+            async with table.batch_writer() as batch:
                 for myDict in jsondata:
-                    response = batch.put_item(Item = myDict)
-
+                    await batch.put_item(Item=myDict)
         else:
             logging.info("[pushPriceHistoryToDynamo] Just update the table")
-            table = self.dynamodb.Table(tableName)
-            itemjson = itemPrices.getItemsPriceHistory(item[tableName].value,region.THE_FORGE.value)
-            jsondata = json.loads(itemjson, parse_float=Decimal)
-            with table.batch_writer() as batch:
+            table = await self.dynamodb.Table(tableName)
+            itemjson = await itemPrices.getItemsPriceHistory(item[tableName].value, region.THE_FORGE.value)
+            jsondata = json.loads(itemjson)
+            async with table.batch_writer() as batch:
                 for myDict in jsondata:
-                    response = batch.put_item(Item = myDict)
-
+                    await batch.put_item(Item=myDict)
+        
         logging.info(f'[pushPriceHistoryToDynamo] Finished pushing the {tableName} table')
-        logging.info(f'[pushPriceHistoryToDynamo] Response code {response}')
 
-    def pushItemOrdersToDynamo(self,tableName: str)->None:
-        stringName = tableName+"_ORDERS"
-        response = self.newTable.createItemOrderTable(stringName)
-        print(stringName)
-        print(response)
+    async def pushItemOrdersToDynamo(self, tableName: str) -> None:
+        loop = asyncio.get_event_loop()
+        stringName = tableName + "_ORDERS"
+        response = await loop.run_in_executor(None, self.newTable.createItemOrderTable, stringName)
+
         if response == 200:
             logging.info(f'[pushItemOrdersToDynamo] Create new table {stringName}')
-            table = self.dynamodb.Table(stringName)
-            table.wait_until_exists()
+            table = await self.dynamodb.Table(stringName)
+            await table.wait_until_exists()
             logging.info("Done waiting")
-            itemjson = itemPrices.getAllItemOrderHistory(item[tableName].value,region.THE_FORGE.value)
-            jsondata = json.loads(itemjson, parse_float=Decimal)
-            with table.batch_writer() as batch:
+            itemjson = await itemPrices.getAllItemOrderHistory(item[tableName].value, region.THE_FORGE.value)
+            jsondata = json.loads(itemjson)
+            async with table.batch_writer() as batch:
                 for myDict in jsondata:
-                    response = batch.put_item(Item = myDict)
+                    await batch.put_item(Item=myDict)
         else:
             logging.info("Just update the table")
-            table = self.dynamodb.Table(stringName)
-            itemjson = itemPrices.getAllItemOrderHistory(item[tableName].value,region.THE_FORGE.value)
-            jsondata = json.loads(itemjson, parse_float=Decimal)
-            with table.batch_writer() as batch:
+            table = await self.dynamodb.Table(stringName)
+            itemjson = await itemPrices.getAllItemOrderHistory(item[tableName].value, region.THE_FORGE.value)
+            jsondata = json.loads(itemjson)
+            async with table.batch_writer() as batch:
                 for myDict in jsondata:
-                    response = batch.put_item(Item = myDict)
-
-
+                    await batch.put_item(Item=myDict)
+        
         logging.info(f'[pushItemOrdersToDynamo] Finished pushing the {tableName}_ORDERS table')
-        logging.info(f'[pushItemOrdersToDynamo] Response code {response}')
--- itemPrices.py
+++ itemPrices.py
@@ -1,28 +1,36 @@
-from mimetypes import init
 from typing import Dict
-import requests
+import aiohttp
 import json
-from ItemIdEnum import item
-from RegionIdEnum import region
 from decimal import Decimal
 
 
+async def getItemsPriceHistory(type_id: int, region_id: int) -> Dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://esi.evetech.net/latest/markets/{region_id}/history/?datasource=tranquility&type_id={type_id}") as response:
+            data = await response.json()
+            return json.dumps(data)
 
-def getItemsPriceHistory(type_id: int,region_id: int) -> Dict:
-    response = requests.get(f"https://esi.evetech.net/latest/markets/{region_id}/history/?datasource=tranquility&type_id={type_id}")
-    return json.dumps(response.json())
 
-def getAllItemOrderHistory(type_id: int,region_id: int) -> Dict:
-    response = requests.get(f"https://esi.evetech.net/latest/markets/{region_id}/orders/?datasource=tranquility&order_type=all&page=1&type_id={type_id}")
-    return json.dumps(response.json())
+async def getAllItemOrderHistory(type_id: int, region_id: int) -> Dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://esi.evetech.net/latest/markets/{region_id}/orders/?datasource=tranquility&order_type=all&page=1&type_id={type_id}") as response:
+            data = await response.json()
+            return json.dumps(data)
 
-# This will need to be turned into an async function and even recursive async
-def getRegionOrderIds(region_id: int) -> list:
-    pageNum = 1
+
+async def getRegionOrderIds(region_id: int) -> list:
     idList = []
-    response = requests.get(f"https://esi.evetech.net/latest/markets/{region_id}/types/?datasource=tranquility&page={pageNum}")
-    while response != 404:
-        response = requests.get(f"https://esi.evetech.net/latest/markets/{region_id}/types/?datasource=tranquility&page={pageNum}")
-        idList.append(response.json())
-        pageNum+=1
+    pageNum = 1
+
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.get(f"https://esi.evetech.net/latest/markets/{region_id}/types/?datasource=tranquility&page={pageNum}") as response:
+                if response.status == 404:
+                    break
+
+                data = await response.json()
+                idList.append(data)
+
+                pageNum += 1
+
     return idList
