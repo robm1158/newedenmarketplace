a9b91ff3c1162f001448e09d5236e72a8f265da8
robm1158 <mullins097956@gmail.com>
2023-09-18 05:49:58 +0000

    adding more docker vars

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ import asyncio
 import aiohttp
 import itemPrices
 
-# # 
+# 
 
-# async def main2():
-#     # List of type_ids
-#     type_ids = ItemIdEnum.item
-#     region_id = RegionIdEnum.region.THE_FORGE.value
+async def main2():
+    # List of type_ids
+    type_ids = ItemIdEnum.item
+    region_id = RegionIdEnum.region.THE_FORGE.value
 
-#     # Gather all asynchronous calls into one, effectively running them concurrently
-#     results = await asyncio.gather(*(itemPrices.getItemsPriceHistory(type_id.value, region_id) for type_id in type_ids))
+    # Gather all asynchronous calls into one, effectively running them concurrently
+    results = await asyncio.gather(*(itemPrices.getItemsPriceHistory(type_id.value, region_id) for type_id in type_ids))
 
-#     print(results)
+    print(results)
 
-# async def main():
-#     manager =  dynamodbPushData.PushData()
+async def main():
+    manager =  dynamodbPushData.PushData()
     
-#     table_names = list(ItemIdEnum.item)
+    table_names = list(ItemIdEnum.item)
 
-#     # Run pushPriceHistoryToDynamo concurrently for all table names
-#     await asyncio.gather(*(manager.pushItemOrdersToDynamo(str(table_name.name)) for table_name in table_names))
+    # Run pushPriceHistoryToDynamo concurrently for all table names
+    await asyncio.gather(*(manager.pushItemOrdersToDynamo(str(table_name.name)) for table_name in table_names))
 
 
 # # Run the event loop
 # if __name__ == "__main__":
-#     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+#     # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 #     asyncio.run(main())
