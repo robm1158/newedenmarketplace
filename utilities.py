7179a191026a9f14667f78708c0a3d4c676dae75
robm1158 <mullins097956@gmail.com>
2023-09-22 02:59:30 -0600

    adding more functions to util

--- utilities.py
+++ utilities.py
@@ -14,10 +14,21 @@ def convertFromZuluTime(df: pd.DataFrame) -> pd.DataFrame:
     df['issued'] = pd.DatetimeIndex(df['issued'])
     return df
 
-def seperateJitaDataTocsv(df: pd.DataFrame, path: pathlib, regionId: region):
+def seperateJitaDataTocsv(df: pd.DataFrame, path: pathlib.Path, regionId: int) -> None:
     newCSVName = path.parent.joinpath("the-forge-historical-" + path.name)
     df[df['region_id'] == regionId].to_csv(newCSVName, index=False)
     
-def seperateItemDataTocsv(df: pd.DataFrame, path: pathlib, item: item):
-    newCSVName = path.parent.joinpath({item.name} + path.name)
+def seperateItemDataTocsv(df: pd.DataFrame, path: pathlib.Path, item: item) -> None:
+    newCSVName = path.parent.joinpath(item.name+'-' + path.name)
+
     df[df['type_id'] == item.value].to_csv(newCSVName, index=False)
+    
+def combineItemcsv(path: pathlib.Path, item: item) -> None:
+    df_merged = pd.DataFrame()
+    newCSVName = pathlib.Path()
+    for file in path.rglob(item.name + "*.csv*"):
+        if str(file.suffix) == ".bz2":
+            df = pd.read_csv(file)
+            df_merged = pd.concat([df_merged,df])
+        newCSVName = path.parent.joinpath("merged-"+item.name+'-' + file.name)
+    df_merged.to_csv(newCSVName, index=False)
