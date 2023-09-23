d26a92403af13dbc5f256be5319edecf8d1ac606
robm1158 <mullins097956@gmail.com>
2023-09-23 01:54:58 -0600

    got the first pridciton model working

--- createLSTM.py
+++ createLSTM.py
@@ -9,6 +9,9 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import TimeSeriesSplit
+import seaborn as sns
+from statsmodels.graphics.gofplots import qqplot
+from scipy.stats import norm, uniform
 
 
 pd.set_option('display.max_rows', 10)
@@ -34,25 +37,30 @@ data = utilities.removeOutliers(data,3)
 
 data = data.drop(columns=['range','universe_id','http_last_modified'])
 print(data)
-data['issued'] = pd.to_datetime(data['issued'], format='%d.%m.%Y %H:%M:%S')
-print(data)
+data['issued'] = pd.to_datetime(data['issued'])
+# print(data)
 date_time = pd.to_datetime(data['issued'], format='%d.%m.%Y %H:%M:%S').unique()
 # data = data.set_index('issued')
-# targetY = data['price']
-# xFeat = data.iloc[:,0:12]
+targetY = data['price']
+xFeat = data.iloc[:,3:12]
+print(xFeat)
+sns.displot(xFeat['price'])
+plt.rcParams['figure.figsize'] = [8, 6]
+qqplot(xFeat['price'], norm, fit=True, line="45")
+
+plt.show()
 # sc =StandardScaler()
 # X_ft = sc.fit_transform(xFeat.values)
 # X_ft = pd.DataFrame(X_ft, index=xFeat.index, columns=xFeat.columns)
 
-# X1,y1 = lstm_split(X_ft.values,10)
-# trainSplit = 0.8
-# splitIDX = int(np.ceil(len(X1)*trainSplit))
-# dateIndex = X_ft.index
-# XTrain, xTest = X1[:splitIDX], X1[splitIDX:]
-# yTrain, yTest = y1[:splitIDX], y1[splitIDX:]
-# XTrainDates, xTestDates = dateIndex[:splitIDX], dateIndex[splitIDX:]
-# print(XTrain.shape,xTest.shape,yTrain.shape, yTest.shape)
-# print(len(date_time))
+X1,y1 = lstm_split(xFeat.values,10)
+trainSplit = 0.8
+splitIDX = int(np.ceil(len(X1)*trainSplit))
+dateIndex = xFeat.index
+XTrain, xTest = X1[:splitIDX], X1[splitIDX:]
+yTrain, yTest = y1[:splitIDX], y1[splitIDX:]
+XTrainDates, xTestDates = dateIndex[:splitIDX], dateIndex[splitIDX:]
+print(XTrain.shape,xTest.shape,yTrain.shape, yTest.shape)
 
 # lstm = tf.keras.models.Sequential()
 # lstm.add(tf.keras.layers.LSTM(50,return_sequences=True,input_shape=(XTrain.shape[1],XTrain.shape[2]),activation='relu'))
@@ -63,11 +71,20 @@ date_time = pd.to_datetime(data['issued'], format='%d.%m.%Y %H:%M:%S').unique()
 # lstm.compile(loss='mean_squared_error',optimizer='adam', metrics='accuracy')
 # lstm.summary()
 
-# history = lstm.fit(XTrain,yTrain,epochs=100,batch_size=2,shuffle=False,validation_data=(xTest,yTest))
+# history = lstm.fit(XTrain,yTrain,epochs=20,batch_size=2,shuffle=False,validation_data=(xTest,yTest))
 # loss, acc = lstm.evaluate(xTest,yTest,verbose=0)
 # print('Loss: %.3f, Acc: %.3f' % (loss, acc))
 
 # y_predict = lstm.predict(xTest)
+# X_test_list = []
+# for i in range(len(xTest)):
+#     X_test_list.append(xTest[i][0])
+    
+# print(len(y_predict),len(X_test_list))
+# test_predictions_df1 = pd.DataFrame({'x_test':list(X_test_list),
+#                                      'LSTM':list(y_predict.flatten())})
+# print(test_predictions_df1.head())
+# test_predictions_df1.plot()
 # print(XTrain.shape,xTest.shape,yTrain.shape, yTest.shape)
 # print(len(xTest[0]))
 # print(yTest.shape)
@@ -79,7 +96,7 @@ date_time = pd.to_datetime(data['issued'], format='%d.%m.%Y %H:%M:%S').unique()
 # plt.ylabel('prediction')
 # plt.xlabel('epoch')
 # plt.legend()
-# plt.show()
+plt.show()
 
 # print(history.history.keys())
 # plt.plot(history.history['loss'],label='loss')
