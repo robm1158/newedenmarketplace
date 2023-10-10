10b8aaba9a2506f77008b884e6b93be7ceaa453b
robm1158 <mullins097956@gmail.com>
2023-10-10 07:08:15 +0000

    changing the directory contents

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,30 @@
 from flask import Flask, jsonify
-# Import CORS
 from flask_cors import CORS
+import pymongo
+import sys
+print(sys.path)
+sys.path.append('/root/code/utilities')
+print(sys.path)
+from utilities.s3PullData import *
+import pandas as pd
 
 app = Flask(__name__)
 
 # Use CORS with your app
 CORS(app)
 
+db = mongoData('eve-market-order-history-the-forge')
+
 @app.route('/')
 def index():
     return jsonify(message="Hello from Flask!")
+
+@app.route('/get_item/<item_name>')
+def get_column(item_name: str):
+    df = db.pullData(item_name)
+    if df is not None:
+        return df.to_json(orient="list"), 200
+    return jsonify({"error": "Data not found for the given ID"}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
