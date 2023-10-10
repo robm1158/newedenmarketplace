009b51e359e841822e31aeec2a74d4c405f115ba
robm1158 <mullins097956@gmail.com>
2023-10-10 16:05:42 +0000

    starting the foundation for a website

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pymongo
 import sys
+
 sys.path.append('/root/code/eve-aws')
 print(sys.path)
 from utils import mongodbData as mdb
@@ -28,6 +29,29 @@ def get_item(item_name: str):
     if df is not None:
         return df.to_json(orient="records"), 200
     return jsonify({"error": "Data not found for the given ID"}), 404
+
+
+@app.route('/get_graph_data', methods=['POST'])
+def get_graph_data():
+    # Extract selected value from request
+    data = request.json
+    selected_value = data.get('selectedValue')
+    if not selected_value:
+        return jsonify({"error": "selectedValue not provided in the request."}), 400
+
+    print(f"Selected value: {selected_value}")
+
+    # Process data based on selected value...
+    df = db.syncPullData(selected_value)
+    if df is None:
+        return jsonify({"error": "No data found for the selected value."}), 404
+
+    df = df.sort_values(by=['issued'], ascending=False)
+    print(df)
+
+    return df.to_json(orient="records"), 200
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
