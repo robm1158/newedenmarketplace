92e8d66a4443e9bbd1dd16551153a498dfccd003
robm1158 <mullins097956@gmail.com>
2023-10-10 16:10:39 +0000

    starting the foundation for a website

---
+++ frontend/src/components/PagedTable/PagedTable.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function PagedTable({ data, rowsPerPage = 10, headers = [] }) {
+    const [currentPage, setCurrentPage] = useState(0);
+    
+    const totalPages = Math.ceil(data.length / rowsPerPage);
+    
+    const currentData = data.slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        {headers.map((header, index) => (
+                            <th key={index}>{header}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {currentData.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {headers.map((header, colIndex) => (
+                                <td key={colIndex}>{row[header]}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div>
+                {currentPage > 0 && <button onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>}
+                {currentPage < totalPages - 1 && <button onClick={() => setCurrentPage(currentPage + 1)}>Next</button>}
+            </div>
+        </div>
+    );
+}
+
+export default PagedTable;
---
+++ frontend/src/constants/ItemEnum.js
@@ -0,0 +1,13 @@
+export const ItemEnum = {
+    PLAGIOCLASE : 18,
+    SPODUMAIN : 19,
+    KERNITE : 20,
+    HEDBERGITE : 21,
+    ARKONOR : 22,
+    TRITANIUM : 34,
+    PYERITE : 35,
+    MEXALLON : 36,
+    ISOGEN : 37,
+    NOCXIUM	: 38
+
+};
