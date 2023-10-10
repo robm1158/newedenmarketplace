995badbb846b42a4357b63ae15e6b69e42f17a93
robm1158 <mullins097956@gmail.com>
2023-10-10 16:05:42 +0000

    starting the foundation for a website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import Dropdown from './components/Dropdown/Dropdown';
 import DataTable from './components/DataTable/DataTable';
+// import DashIframe from './components/DashIframe/DashIframe';
+import Graph from './components/Graph/Graph';
+import { ItemEnum } from './constants/ItemEnum';
+import PagedTable from './components/PagedTable/PagedTable';
 import axios from 'axios';
 import './App.css';
 
-// Import the enum
-import { ItemEnum } from './constants/ItemEnum';
 
 function App() {
-  const [data, setData] = useState(null);  
+  const [tableData, setTableData] = useState(null);
+  const [graphData, setGraphData] = useState(null);
 
   // Convert the Enum to dropdown options format using the keys
   const options = Object.keys(ItemEnum).map(key => ({
@@ -16,12 +19,27 @@ function App() {
     value: key,
   }));
 
+  // These are moved outside of the handleDropdownChange function
+  const buyOrders = tableData ? tableData.filter(order => order.is_buy_order === true) : [];
+  const nonBuyOrders = tableData ? tableData.filter(order => order.is_buy_order === false) : [];
+  console.log("buy orders*:", buyOrders);
+  console.log("non buy orders*:", nonBuyOrders);
+
   const handleDropdownChange = async (selectedValue) => {
-    console.log("Selected value:", selectedValue);
+    console.log("Selected value*:", selectedValue);
+    console.log("Graph data*:", graphData);
+
     try {
-      const response = await axios.get(`http://127.0.0.1:5000/get_item/${selectedValue}`);
-      setData(response.data);
-      console.log("Fetched data:", response.data);
+      // Endpoint for fetching table data
+      const tableResponse = await axios.get(`http://127.0.0.1:5000/get_item/${selectedValue}`);
+      setTableData(tableResponse.data);
+      console.log("Table data:", tableData);
+
+      // Endpoint for fetching graph data - using POST as expected by Flask
+      const graphResponse = await axios.post(`http://127.0.0.1:5000/get_graph_data`, { selectedValue: selectedValue });
+      setGraphData(graphResponse.data);
+      console.log("Graph data:", graphData);
+
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -29,12 +47,24 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My React App</h1>
-      <Dropdown options={options} onChange={handleDropdownChange} />
-      {data && data.length > 0 && <DataTable data={data} />}
-      <a href="/dashboard/" target="_blank">Go to Dashboard</a>
+        <h1>My React App</h1>
+        <Dropdown options={options} onChange={handleDropdownChange} />
+        
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div>
+                <h2>Buy Orders</h2>
+                <PagedTable data={buyOrders} headers={['id', 'name', 'price']} />
+            </div>
+            <div>
+                <h2>Non-Buy Orders</h2>
+                <PagedTable data={nonBuyOrders} headers={['id', 'name', 'price']} />
+            </div>
+        </div>
+
+        {graphData && <Graph data={graphData} />}
     </div>
-  );
+);
+
 }
 
 export default App;
