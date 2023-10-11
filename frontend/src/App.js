78a522aeb95157a00ba6e8588c37c3c58b3fb7a1
robm1158 <mullins097956@gmail.com>
2023-10-11 08:27:22 +0000

    adding more for stuff for webstie

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,38 @@
 import React, { useState } from 'react';
 import Dropdown from './components/Dropdown/Dropdown';
-import DataTable from './components/DataTable/DataTable';
-// import DashIframe from './components/DashIframe/DashIframe';
+import { Routes, Route } from 'react-router-dom';
 import Graph from './components/Graph/Graph';
 import { ItemEnum } from './constants/ItemEnum';
 import PagedTable from './components/PagedTable/PagedTable';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
+import CustomSidebar from "./scenes/global/Sidebar";
+// import { Dashboard } from "./scenes/dashboard";
 import axios from 'axios';
 import './App.css';
-
 
 function App() {
   const [tableData, setTableData] = useState(null);
   const [graphData, setGraphData] = useState(null);
   const [theme, colorMode] = useMode();
 
-  // Convert the Enum to dropdown options format using the keys
   const options = Object.keys(ItemEnum).map(key => ({
     label: key,
     value: key,
   }));
 
-  // These are moved outside of the handleDropdownChange function
   const buyOrders = tableData ? tableData.filter(order => order.is_buy_order === true) : [];
   const nonBuyOrders = tableData ? tableData.filter(order => order.is_buy_order === false) : [];
-  console.log("buy orders*:", buyOrders);
-  console.log("non buy orders*:", nonBuyOrders);
 
   const handleDropdownChange = async (selectedValue) => {
-    console.log("Selected value*:", selectedValue);
-    console.log("Graph data*:", graphData);
-
     try {
-      // Endpoint for fetching table data
       const tableResponse = await axios.get(`http://127.0.0.1:5000/get_item/${selectedValue}`);
       setTableData(tableResponse.data);
-      console.log("Table data:", tableData);
 
-      // Endpoint for fetching graph data - using POST as expected by Flask
       const graphResponse = await axios.post(`http://127.0.0.1:5000/get_graph_data`, { selectedValue: selectedValue });
       setGraphData(graphResponse.data);
-      console.log("Graph data:", graphData);
-
+      
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -53,15 +43,21 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="App">
+          <div className="sidebar">
+            <CustomSidebar />
+          </div>
           <main className='content'>
+            <Topbar />
+            {/* <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes> */}
             <h1>My React App</h1>
             <Dropdown options={options} onChange={handleDropdownChange} />
-            <div style={{ display: "flex", justifyContent: 'center'}}>
+            <div style={{ display: "flex", justifyContent: 'center' }}>
               {graphData && <Graph data={graphData} />}
             </div>
             
             <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-              
                 <div>
                     <h2>Buy Orders</h2>
                     <PagedTable data={buyOrders} headers={['issued', 'station_id', 'price']} />
@@ -75,8 +71,7 @@ function App() {
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
-);
-
+  );
 }
 
 export default App;
--- itemPrices.py
+++ itemPrices.py
@@ -1,7 +1,10 @@
 from typing import Dict
 import aiohttp
+import asyncio
 import json
 from decimal import Decimal
+import requests
+from tqdm import tqdm
 
 # an async function that kicks off requests to EVE ESI/API and returns that items price history
 async def getItemsPriceHistory(type_id: int, region_id: int) -> Dict:
@@ -34,3 +37,55 @@ async def getRegionOrderIds(region_id: int) -> list:
                 pageNum += 1
 
     return idList
+
+async def getGroups() -> list:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://esi.evetech.net/latest/markets/groups/") as response:
+            data = await response.json()
+            return data
+        
+
+async def fetch(session, group):
+    url = f"https://esi.evetech.net/latest/markets/groups/{group}/?datasource=tranquility&language=en"
+    async with session.get(url) as response:
+        return await response.json()
+
+async def unravelGroupsAsync(data: list):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch(session, group) for group in data]
+        return await asyncio.gather(*tasks)
+
+def unravelGroups(data: list):
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(unravelGroupsAsync(data))
+    return result
+
+
+def construct_hierarchy(data):
+    def find_children(parent_id):
+        children = []
+        for item in tqdm(data, total=100):
+            if item.get('parent_group_id') == parent_id:
+                child = {
+                    "description": item["description"],
+                    "market_group_id": item["market_group_id"],
+                    "name": item["name"],
+                    "types": item["types"]
+                }
+                child['children'] = find_children(item["market_group_id"])
+                children.append(child)
+        return children
+
+    root_items = [item for item in data if "parent_group_id" not in item]
+    hierarchy = []
+
+    for root in tqdm(root_items, total=100):
+        root_data = {
+            "description": root["description"],
+            "market_group_id": root["market_group_id"],
+            "name": root["name"],
+            "types": root["types"],
+            "children": find_children(root["market_group_id"])
+        }
+        hierarchy.append(root_data)
+    return hierarchy
