d00a1c0d39111657ccce2cdbd44c8ba6408d532e
robm1158 <mullins097956@gmail.com>
2023-10-14 07:24:21 +0000

    added a new bubble graph based on profit and order count

--- itemPrices.py
+++ itemPrices.py
@@ -3,17 +3,24 @@ import aiohttp
 import json
 from decimal import Decimal
 
+header = {
+    "In-Game-Name": "XiT Statik Daphiti",
+    "Discord-Name": "xit_statik",
+    "email": "mullins097956@gmail.com",
+    "application": "EVE-AWS"
+}
+
 # an async function that kicks off requests to EVE ESI/API and returns that items price history
 async def getItemsPriceHistory(type_id: int, region_id: int) -> Dict:
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://esi.evetech.net/latest/markets/{region_id}/history/?datasource=tranquility&type_id={type_id}") as response:
+        async with session.get(f"https://esi.evetech.net/latest/markets/{region_id}/history/?datasource=tranquility&type_id={type_id}",headers=header) as response:
             data = await response.json()
-            return json.dumps(data) # type: ignore
+            return data # type: ignore
 
 # An async function that kicks off requests to EVE ESI/API that returns that items current orders
 async def getAllItemOrderHistory(type_id: int, region_id: int) -> Dict:
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://esi.evetech.net/latest/markets/{region_id}/orders/?datasource=tranquility&order_type=all&page=1&type_id={type_id}") as response:
+        async with session.get(f"https://esi.evetech.net/latest/markets/{region_id}/orders/?datasource=tranquility&order_type=all&page=1&type_id={type_id}",headers=header) as response:
             data = await response.json()
             return json.dumps(data)  # type: ignore
 
@@ -24,7 +31,7 @@ async def getRegionOrderIds(region_id: int) -> list:
 
     async with aiohttp.ClientSession() as session:
         while True:
-            async with session.get(f"https://esi.evetech.net/latest/markets/{region_id}/types/?datasource=tranquility&page={pageNum}") as response:
+            async with session.get(f"https://esi.evetech.net/latest/markets/{region_id}/types/?datasource=tranquility&page={pageNum}",headers=header) as response:
                 if response.status == 404:
                     break
 
