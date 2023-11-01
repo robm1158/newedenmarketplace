c97ada214275c981bbd8fe75060553530cf39840
robm1158 <mullins097956@gmail.com>
2023-11-01 07:41:40 +0000

    reworking how the main page looks added home button

--- frontend/src/scenes/global/Topbar.jsx
+++ frontend/src/scenes/global/Topbar.jsx
@@ -5,30 +5,35 @@ import InputBase from '@mui/material/InputBase';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import SpaceDashboardOutlinedIcon from '@mui/icons-material/SpaceDashboardOutlined';
-import { useNavigate } from 'react-router-dom'; // <-- Import the useNavigate hook
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'; // <-- Import the new icon
+import { useNavigate } from 'react-router-dom';
 
 const Topbar = ({ ...props }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
-    const navigate = useNavigate(); // <-- Declare the navigate function
+    const navigate = useNavigate();
 
     return (
         <Box display="flex" justifyContent="space-between" p={2}> 
-            {/* Search Bar */}
-            <Box display="flex" backgroundColor={colors.primary[300]} borderRadius="3">
-                <InputBase sx={{ml: 2, flex: 1}} placeholder="Search..." />
-                <IconButton type="button" sx={{p: 1}}>
-                    <SearchOutlinedIcon />
+            {/* Home and Search Bar */}
+            <Box display="flex" alignItems="center"> 
+                <IconButton onClick={() => navigate("/")}>
+                    <HomeOutlinedIcon /> {/* Home icon that navigates to the home route */}
                 </IconButton>
+                <Box display="flex" backgroundColor={colors.primary[300]} borderRadius="3" ml={1}>
+                    <InputBase sx={{ml: 2, flex: 1}} placeholder="Search..." />
+                    <IconButton type="button" sx={{p: 1}}>
+                        <SearchOutlinedIcon />
+                    </IconButton>
+                </Box>
             </Box>
             {/* Icons */}
             <Box display="flex">
                 <IconButton onClick={() => navigate("/dashboard")}>
-                    <SpaceDashboardOutlinedIcon /> {/* This icon will be used for the dashboard navigation button */}
+                    <SpaceDashboardOutlinedIcon />
                 </IconButton>
                 <IconButton>
                     <NotificationsOutlinedIcon />
