88e489e25befa6d57bde7091bc3e81db78df3e2c
robm1158 <mullins097956@gmail.com>
2023-11-02 11:05:22 +0000

    trying to fix deployment stuff

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -4,11 +4,11 @@ import image from '../../assets/images/portrait.jpg'
 
 function Home() {
     const [homeData, setHomeData] = useState(null);
-    const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+    
     useEffect(() => {
         async function fetchHomeData() {
             try {
-                const response = await axios.get(`${BACKEND_URL}/`);
+                const response = await axios.get('http://127.0.0.1:5000/');
                 setHomeData(response.data);
             } catch (error) {
                 console.error("Error fetching home data:", error);
@@ -20,23 +20,13 @@ function Home() {
 
     return (
         <div>
-            {homeData ? (
-                <div>
-                    
-                    <h1>Welcome to the Homepage</h1>
-                    <img src={image} alt="Character Portrait" />
-                    {/* Other content of your homepage */}
                 
-                </div>
-        //         <div>
-        //             <video width="100%" height="auto" controls>
-        //         <source src={videoSource} type="video/mp4" />
-        //         Your browser does not support the video tag.
-        //     </video>
-        // </div>
-            ) : (
-                <div>Loading...</div>
-            )}
+                    
+            <h1>Welcome to the Homepage</h1>
+            <img src={image} alt="Character Portrait" />
+            {/* Other content of your homepage */}
+                
+    
         </div>
     );
 }
